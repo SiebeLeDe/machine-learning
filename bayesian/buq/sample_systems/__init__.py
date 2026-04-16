7ead7f8696d4df9ba6e9cc23b6aee1ff422d4ffb
SiebeLeDe <s.j.lekanne.deprez@vu.nl>
2026-04-16 15:05:59 +0200

    Added bayesian BUQ files

---
+++ bayesian/buq/sample_systems/__init__.py
@@ -0,0 +1,2 @@
+from .mock import Mock1DSystem, Mock2DSystem, Adipep1DFromGrid, Adipep2DFromGrid
+__all__ = ["Mock1DSystem", "Mock2DSystem", "Adipep1DFromGrid", "Adipep2DFromGrid"]
---
+++ ml-intro/README.md
@@ -0,0 +1,1 @@
+Part of Bernd Ensing on April 15th and 17th 2026
